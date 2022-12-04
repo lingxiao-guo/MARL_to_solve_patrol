9c2164a020629f4e78f9c21d62efc28e9a596127
glx15534565855 <98088873+glx15534565855@users.noreply.github.com>
2022-12-04 21:28:01 +0800

    MARL strategy for multi-robot patrol

---
+++ MARL_patrol/Env/SupplementforEnv/obstacle3.py
@@ -0,0 +1,402 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 11:14:44 2020
+
+@author: amris
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 11:14:44 2020
+
+@author: amris
+"""
+import math
+
+import skgeom as sg
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+sys.path.append("..")
+from Env.SupplementforEnv.constants import CONSTANTS as K
+from matplotlib.path import Path
+from collections import defaultdict
+from functools import partial
+from collections import defaultdict
+from heapq import *
+CONST = K()
+
+import time
+
+from Env.SupplementforEnv.vsb2 import Visibility
+
+
+class Obstacle:
+    def __init__(self):
+
+
+        self.nodes={0:[5,0],1:[14,0],2:[23,0],3:[32,0],4:[41,0],5:[5,9],6:[14,9],7:[23,9],8:[32,9],9:[41,9],10:[5,18],
+                    11:[14,18],12:[23,18],13:[32,18],14:[41,18],15:[5,27],16:[14,27],
+                    17:[23,27],18:[32,27],19:[41,27],20:[5,36],21:[14,36],22:[23,36],23:[32,36],24:[41,36]}
+        self.patrol_nodes=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+
+    def getAllObs_vsbs(self, emptyMap):
+        obsMaps = []
+        vsbs = []
+        vsbPolys = []
+        numOpenCellsArr = []
+        a = time.time()
+
+        #        mp, vsb = self.getObstacleMap(emptyMap, self.obstacle1())
+        #        obsMaps.append(mp)
+        #        vsbs.append(vsb)
+        #        vsbPoly =  self.getVisibilityPolys(vsb, mp)
+        #        vsbPolys.append(vsbPoly)
+        #        numOpenCellsArr.append(np.count_nonzero(mp==0))
+        #
+        #        mp, vsb = self.getObstacleMap(emptyMap, self.obstacle2())
+        #        obsMaps.append(mp)
+        #        vsbs.append(vsb)
+        #        vsbPoly =  self.getVisibilityPolys(vsb, mp)
+        #        vsbPolys.append(vsbPoly)
+        #        numOpenCellsArr.append(np.count_nonzero(mp==0))
+        #
+        #        mp, vsb = self.getObstacleMap(emptyMap, self.obstacle3())
+        #        obsMaps.append(mp)
+        #        vsbs.append(vsb)
+        #        vsbPoly =  self.getVisibilityPolys(vsb, mp)
+        #        vsbPolys.append(vsbPoly)
+        #        numOpenCellsArr.append(np.count_nonzero(mp==0))
+        #
+        #        mp, vsb = self.getObstacleMap(emptyMap, self.obstacle4())
+        #        obsMaps.append(mp)
+        #        vsbs.append(vsb)
+        #        vsbPoly =  self.getVisibilityPolys(vsb, mp)
+        #        vsbPolys.append(vsbPoly)
+        #        numOpenCellsArr.append(np.count_nonzero(mp==0))
+
+        mp, vsb = self.getObstacleMap(emptyMap, self.obstacle6())
+        obsMaps.append(mp)
+        vsbs.append(vsb)
+        vsbPoly = self.getVisibilityPolys(vsb, mp)
+        vsbPolys.append(vsbPoly)
+        numOpenCellsArr.append(np.count_nonzero(mp == 0))
+        self.get_allnodes_matrix()
+        b = time.time()
+        print("create vsb Polys:", round(1000 * (b - a), 3))
+        return obsMaps, vsbs, vsbPolys, numOpenCellsArr
+
+    def getObstacleMap(self, emptyMap, obstacleSet):
+        obsList = obstacleSet
+        vsb = Visibility(emptyMap.shape[0], emptyMap.shape[1])
+        for obs, isHole in obsList:
+            vsb.addGeom2Arrangement(obs)
+
+        isHoles = [obs[1] for obs in obsList]
+        if any(isHoles) == True:
+            pass
+        else:
+            vsb.boundary2Arrangement(vsb.length, vsb.height)
+
+        # get obstacle polygon
+        points = CONST.GRID_CENTER_PTS
+        img = np.zeros_like(emptyMap, dtype=bool)
+        for obs, isHole in obsList:
+            p = Path(obs)
+            grid = p.contains_points(points)
+            mask = grid.reshape(CONST.MAP_SIZE, CONST.MAP_SIZE)
+            img = np.logical_or(img, (mask if not isHole else np.logical_not(mask)))
+
+        img = img.T
+        img = np.where(img, 150, emptyMap)
+        return img, vsb
+
+    def getVisibilityPolys(self, vsb, obsMap):
+        polys = defaultdict(partial(np.ndarray, 0))
+        for pt in CONST.GRID_CENTER_PTS:
+            if not obsMap[int(pt[0]), int(pt[1])] == 150:
+                polys[(pt[0], pt[1])] = vsb.getVsbPoly(pt)
+
+        return polys
+
+    def getObstacles(self):
+        obstacle = self.obstacle1()
+        return obstacle
+
+    def get_allnodes_matrix(self):
+
+
+        neighbor_matrix=np.zeros((25,25),np.int32)
+        neighbor_matrix[0,1]=9
+        neighbor_matrix[0, 5] = 9
+        neighbor_matrix[1,6]=9
+        neighbor_matrix[2, 7] = 9
+        neighbor_matrix[2, 3] = 9
+        neighbor_matrix[3, 8] = 9
+        neighbor_matrix[3, 4] = 9
+        neighbor_matrix[4,9] = 9
+        neighbor_matrix[5, 6] = 9
+        neighbor_matrix[5, 10] = 9
+        neighbor_matrix[6, 7] = 9
+        neighbor_matrix[6, 11] = 9
+        neighbor_matrix[7, 8] = 9
+        neighbor_matrix[7, 12] = 9
+        neighbor_matrix[8, 9] = 9
+        neighbor_matrix[8, 13] = 9
+        neighbor_matrix[9, 14] = 9
+        neighbor_matrix[10, 11] = 9
+        neighbor_matrix[11, 12] = 9
+        neighbor_matrix[11, 16] = 9
+        neighbor_matrix[12, 17] = 9
+        neighbor_matrix[13, 14] = 9
+        neighbor_matrix[13, 18] =9
+        neighbor_matrix[14, 19] = 9
+        neighbor_matrix[15, 16] = 9
+        neighbor_matrix[15, 20] = 9
+        neighbor_matrix[16, 21] = 9
+        neighbor_matrix[16, 17] = 9
+        neighbor_matrix[17, 22] = 9
+        neighbor_matrix[17, 18] = 9
+        neighbor_matrix[18, 23] = 9
+        neighbor_matrix[18, 19] = 9
+        neighbor_matrix[19, 24] = 9
+        neighbor_matrix[20, 21] = 9
+        neighbor_matrix[21, 22] = 9
+        neighbor_matrix[22, 23] = 9
+        neighbor_matrix[23, 24] = 9
+
+
+        self.neighbor_matrix=neighbor_matrix+neighbor_matrix.T
+        self.neighbor_matrix=np.where(self.neighbor_matrix==0,math.inf,self.neighbor_matrix)
+        for i in range(np.size(self.neighbor_matrix,0)):
+            self.neighbor_matrix[i][i]=0
+
+        self.edges = []
+        for i in range(np.size(self.neighbor_matrix,0)):
+            for j in range(np.size(self.neighbor_matrix,1)):
+                if i != j and self.neighbor_matrix[i][j] != math.inf:
+                    self.edges.append(
+                        (i, j, self.neighbor_matrix[i][j]))  ### (i,j) is a link; M_topo[i][j] here is 1, the length of link (i,j).
+        adj=np.zeros((25,25),dtype=int)
+
+
+        adj=np.where(self.neighbor_matrix==9,1,adj)
+        self.adj=adj
+
+
+
+
+    def get_key(self,val,my_dict):
+        for key, value in my_dict.items():
+            if val[0]==value[0] and val[1]==value[1]:
+                return key
+
+    def get_neighbor_ptnodes(self,node_pos):
+        neighbor_nodes=[]
+
+        ptnode=self.get_key(node_pos,self.nodes)
+        idx=np.nonzero(self.adj[int(ptnode)])
+        for item in idx[0]:
+            neighbor_nodes.append(item)
+
+        nb_nodes=[]
+        for i in range(len(neighbor_nodes)) :
+            nb_nodes.append(self.nodes[neighbor_nodes[i]])
+        return nb_nodes
+
+    def get_neighbor_nodes_number(self,ptnode):
+        neighbor_nodes = []
+
+        idx = np.nonzero(self.adj[int(ptnode)])
+        for item in idx[0]:
+            neighbor_nodes.append(item)
+
+        return neighbor_nodes
+
+
+
+
+    def dijkstra_raw(self, from_node,to_node):
+        g = defaultdict(list)
+        for l, r, c in self.edges:
+            g[l].append((c, r))
+        q, seen = [(0, from_node, ())], set()
+        while q:
+            (cost, v1, path) = heappop(q)
+            if v1 not in seen:
+                seen.add(v1)
+                path = (v1, path)
+                if v1 == to_node:
+                    return cost, path
+                for c, v2 in g.get(v1, ()):
+                    if v2 not in seen:
+                        heappush(q, (cost + c, v2, path))
+        return float("inf"), []
+
+    def dijkstra(self, from_node, to_node):
+        len_shortest_path = -1
+        ret_path = []
+        length, path_queue = self.dijkstra_raw(from_node,to_node)
+        if len(path_queue) > 0:
+            len_shortest_path = length  ## 1. Get the length firstly;
+            ## 2. Decompose the path_queue, to get the passing nodes in the shortest path.
+            left = path_queue[0]
+            ret_path.append(left)  ## 2.1 Record the destination node firstly;
+            right = path_queue[1]
+            while len(right) > 0:
+                left = right[0]
+                ret_path.append(left)  ## 2.2 Record other nodes, till the source-node.
+                right = right[1]
+            ret_path.reverse()  ## 3. Reverse the list finally, to make it be normal sequence.
+        return len_shortest_path, ret_path
+
+
+    def patrol_pts(self):
+        pt0 = []
+        for i in range(len(self.patrol_nodes)):
+            pt0.append(self.nodes[self.patrol_nodes[i]][0])
+        pt1 = []
+        for i in range(len(self.patrol_nodes)):
+            pt1.append(self.nodes[self.patrol_nodes[i]][1])
+        pt = pt0, pt1
+
+        return pt
+
+    def obstacle6(self):
+        obsList = []
+        # add points in CW order and
+        isHole = False
+        geom = [[6, 1],
+                [6, 9],
+                [14, 9],
+                [14, 1]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[6, 10],
+                [6, 18],
+                [14, 18],
+                [14, 10]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[6, 28],
+                [6, 36],
+                [14, 36],
+                [14, 28]
+
+                ]
+        obsList.append([geom, isHole])
+        geom = [[15, 10],
+                [15, 18],
+                [23, 18],
+                [23, 10]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[15, 19],
+                [15, 27],
+                [23, 27],
+                [23, 19]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[15, 28],
+                [15, 36],
+                [23, 36],
+                [23, 28]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[24, 1],
+                [24, 9],
+                [32, 9],
+                [32, 1]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[24, 10],
+                [24, 27],
+                [32, 27],
+                [32, 10]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[24, 28],
+                [24, 36],
+                [32, 36],
+                [32, 28]
+
+                ]
+        obsList.append([geom, isHole])
+
+
+        geom = [[33, 1],
+                [33, 9],
+                [41, 9],
+                [41, 1]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[33, 10],
+                [33, 18],
+                [41, 18],
+                [41, 10]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[33, 19],
+                [33, 27],
+                [41, 27],
+                [41, 19]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [[33, 28],
+                [33, 36],
+                [41, 36],
+                [41, 28]
+
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [
+                [15,0],
+                [15,9],
+                [23,9],
+                [23,0]
+                ]
+        obsList.append([geom, isHole])
+
+        geom = [
+            [5, 0],
+            [5, 19],
+            [14, 19],
+            [14, 27],
+            [5,27],
+            [5,37],
+            [42,37],
+            [42,0],
+            [50,0],
+            [50,50],
+            [0,50],
+            [0,0]
+        ]
+        obsList.append([geom, isHole])
+
+        return obsList
+
+obsMap = Obstacle()
+obsMaps, vsbs, vsbPolys, numOpenCellsArr = obsMap.getAllObs_vsbs(np.zeros((CONST.MAP_SIZE, CONST.MAP_SIZE)))
+
+
+
---
+++ MARL_patrol/patrol_policy/sy_patrol/sy_decoder.py
@@ -0,0 +1,298 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from torch import nn
+import math
+import os
+import sys
+FILE_ABSOLUTE_PATH = os.path.abspath(__file__)  # get absolute filepath
+CURRENT_DIR = os.path.dirname(FILE_ABSOLUTE_PATH)  # get directory path of file
+PARENT_DIR = os.path.dirname(CURRENT_DIR)  # get parent directory path
+BASE_DIR = os.path.dirname(PARENT_DIR)  # get grandparent directory path
+
+sys.path.append(BASE_DIR)
+from Env.env import obsMap
+
+
+from typing import NamedTuple
+
+
+from Env.env import CONST
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cpu')
+
+class AttentionModelFixed():
+
+    def __init__(self,node_embeddings,context_node_projected,glimpse_key,glimpse_val,logit_key):
+        self.node_embeddings=node_embeddings
+        self.context_node_projected=context_node_projected
+        self.glimpse_key=glimpse_key
+        self.glimpse_val=glimpse_val
+        self.logit_key=logit_key
+
+
+class AttentionModel(nn.Module):
+
+    def __init__(self,env,args,tanh_clipping=10,normalization='batch',checkpoint_encoder=False):
+        encoder_dim=args.num_hidden
+        hidden_dim=args.num_hidden
+        action_dim=args.action_dim
+        n_heads=args.num_head
+        norm_shape=args.num_hidden
+        select_type=args.select_type
+        super(AttentionModel,self).__init__()
+        self.temp=1
+        self.encoder_dim=encoder_dim
+        self.hidden_dim=hidden_dim
+        self.tanh_clipping=tanh_clipping
+        self.adj=env.adj
+        self.n_heads=n_heads
+        self.checkpoint_encoder=checkpoint_encoder
+        self.select_type=select_type
+        self.ln=nn.LayerNorm(norm_shape)
+        # For each node we compute (glimpse key, glimpse value, logit key) so 3 * embedding_dim
+        self.project_node_embeddings = nn.Linear(encoder_dim, 3 * encoder_dim, bias=False).to(device)
+        self.project_fixed_context = nn.Linear(encoder_dim, encoder_dim, bias=False).to(device)
+        self.project_step_context=nn.Linear(encoder_dim,encoder_dim,bias=False).to(device)
+        # Note n_heads * val_dim == embedding_dim so input to project_out is embedding_dim
+        self.project_out = nn.Linear(encoder_dim, encoder_dim, bias=False).to(device)
+        self.action_embedding=nn.Linear(action_dim,hidden_dim).to(device)
+        self.self_attn=SelfAttention_()
+        self.attn=Attention_()
+
+    def forward(self,encoder_output,node_last_list,decision_index,decision_flag,shift_action=None):
+
+        decoder_input=encoder_output
+
+
+        log_prob,selected,shift_action_total=self._get_log_p(encoder_output,self.adj,node_last_list,shift_action)
+
+        return selected,log_prob,shift_action_total
+
+    def _precompute(self,encoder_output,action_embed):
+        #encoder_output.shape():batch_size,num_agents,graph_size,hidden_dim
+        action_embed=action_embed.unsqueeze(2)
+        graph_size=encoder_output.shape[2]
+        action_embed=torch.repeat_interleave(action_embed,graph_size,dim=2)
+        encoder_output = self.ln(encoder_output + self.attn(encoder_output, action_embed, action_embed))
+        graph_embed=torch.mean(encoder_output,axis=-2)
+        graph_embed=graph_embed.to(device)
+        fixed_context=self.project_fixed_context(graph_embed)[:,:,None,:]
+        #encoder_output=torch.Tensor(encoder_output)
+        # (batch_size,num_agents,1,hidden_dim)
+
+        glimpse_key_fixed,glimpse_val_fixed,logit_key_fixed=\
+            self.project_node_embeddings(encoder_output).chunk(3,dim=-1)
+        # batch_size,num_agents,graph_size,hidden_dim
+        fixed_attention_node_data = (
+            self._make_heads(glimpse_key_fixed),
+            self._make_heads(glimpse_val_fixed),
+            logit_key_fixed.contiguous()
+        )
+
+        return AttentionModelFixed(encoder_output, fixed_context, *fixed_attention_node_data)
+
+    def _make_heads(self, v):
+
+          return (
+              v.contiguous().view(v.size(0), v.size(1), v.size(2), self.n_heads, -1).permute(3, 0, 1, 2, 4)
+          )  # (n_heads,batch_size,num_agents,graph_size,head_hidden_dim)
+
+    def _get_log_p(self,decoder_input,adj,node_last_list,shift_action=None,normalize=True):
+
+          #Compute query=context node embedding
+
+          node_last_list=np.array(node_last_list)
+
+          action_total=[]
+          batch_size=decoder_input.shape[0]
+          graph_size=decoder_input.shape[-2]
+          log_prob_total=torch.zeros((batch_size,CONST.NUM_AGENTS,graph_size)).to(device)
+          istrain=True
+          if shift_action==None:
+             istrain=False
+             shift_action=torch.zeros((batch_size,CONST.NUM_AGENTS,CONST.NUM_AGENTS,2)).to(device)
+             shift_action[:,0,0,0]=1
+          visitflag=[False for i in range(graph_size)]
+
+          for agent in range(CONST.NUM_AGENTS):
+
+              action_embed = self.action_embedding(shift_action[:, agent, :, :])
+              # (batch_size,num_agent,hidden_dim)
+              action_embed = self.self_attn(action_embed, action_embed, action_embed)
+              fixed = self._precompute(decoder_input,action_embed)
+              i, recent_node = self.get_recent_node(fixed.node_embeddings[:,agent,:,:], node_last_list[:,agent])
+
+              query = fixed.context_node_projected[:,agent,:,:] + \
+                      self.project_step_context(recent_node)
+              # query:(batch_size,1,hidden_dim)
+
+
+              #query=self.attn(query,action_embed,action_embed)
+              #query=self.ln(query+self.attn(query,action_embed,action_embed))
+
+              # shift_action: (batch_size,num_agent,action_dim)
+              glimpse_K, glimpse_V, logit_K = fixed.glimpse_key[:,:,agent,:,:], fixed.glimpse_val[:,:,agent,:,:], \
+                                              fixed.logit_key[:,agent,:,:]
+              glimpse_Q = query .view(query.size(0), self.n_heads, 1, query.size(2) // self.n_heads).permute(1, 0, 2, 3)
+
+              # if batch_size==1:
+              # glimpse_Q:(n_heads,batch_size,1,feature_dim//n_heads)
+              # glimpse_V:(n_heads,batch_size,graph_size,feature_dim//n_heads))
+              # glimpse_K:(n_heads,batch_size,graph_size,feature_dim//n_heads))
+              # logit_K:(batch_size,graph_size,hidden_dim)
+              dim = glimpse_K.size(-1)
+              weight = torch.matmul(glimpse_Q, glimpse_K.transpose(-2, -1)) / math.sqrt(dim)
+              # (n_heads,batch_size,1,graph_size)
+              # i:list
+              adj_m = []
+              for k in range(len(i)):
+                  c=i[k]
+                  d=self.adj[c]
+                  adj_m.append(self.adj[i[k]])
+              adj_m = np.array(adj_m, dtype=int)[:, None, :]
+              adj = adj_m
+              np.expand_dims(adj_m, 0).repeat(glimpse_K.shape[0], axis=0)
+              # adj_m:(n_heads,batch_size,1,graph_size)
+              # adj:(batch_size,1,graph_size)
+              adj_m = torch.tensor(adj_m).to(device)
+              adj = torch.tensor(adj).to(device)
+              zero_vec = -9e15 * torch.ones_like(weight)
+              weight = torch.where(adj_m > 0, weight, zero_vec)
+              score = torch.matmul(nn.functional.softmax(weight, dim=-1), glimpse_V)
+              # (n_heads,batch_size,1,feature_dim//n_heads)
+              final_Q = self.project_out(
+                  score.permute(1, 2, 0, 3).contiguous().view(score.shape[1], score.shape[2],
+                                                              self.n_heads * score.shape[-1]))
+              # (batch_size,1,hidden_dim)
+              logits = torch.matmul(final_Q, logit_K.transpose(-2, -1)) / math.sqrt(dim)
+              logits = torch.tanh(logits) * self.tanh_clipping
+              zero_vec = -9e15 * torch.ones_like(logits)
+              logits = torch.where(adj > 0, logits, zero_vec)
+              log_prob = torch.log_softmax(logits / self.temp, dim=-1)
+              log_prob_total[:,agent,:]=log_prob.squeeze()
+              log_prob=log_prob.clone()
+              prob = log_prob.exp()
+              selected,visitflag = self._select_node(prob,visitflag)
+              action_total.append(selected)
+
+
+              if agent<CONST.NUM_AGENTS-1 and not istrain:
+                  shift_action[:,agent+1,:,:]=shift_action[:,agent,:,:].clone()
+                  for i in range(selected.shape[0]):
+                     c=1
+
+                     shift_action[i,agent+1,agent+1,:]=torch.tensor(obsMap.nodes
+                                                                [obsMap.patrol_nodes[selected[i].clone()]] ).to(device)
+                     #默认selected: (batch_size)
+
+                     #shift_action=shift_action.contiguous()
+
+          action_total=torch.stack(action_total).to(device)
+          action_total=action_total.reshape(batch_size,CONST.NUM_AGENTS)
+          return log_prob_total,action_total,shift_action
+
+
+
+
+
+    def get_recent_node(self,node_embedding,node_last_list):
+        i=[]
+        recent_node=[]
+        for k in range(len(node_last_list)):  #k:0~49
+          i.append(obsMap.patrol_nodes.index(node_last_list[k]))
+          recent_node.append(node_embedding[k,i[k],:])
+        recent_node=torch.stack(recent_node).unsqueeze(1)
+        return i,recent_node
+
+    def _select_node(self, probs, visitflag):
+        vf_copy = visitflag.copy()
+        probs = probs.squeeze()
+        probs_copy = probs.clone()
+        # for i in range(len(visitflag)):
+        #    if visitflag[i]:
+        #        probs[i] = 0
+        # flag=False
+        # for i in range(len(probs)):
+        #    if probs[i]!=0:
+        #        flag=True
+        # if not flag:
+        #   probs=probs_copy
+        if self.select_type == 'greedy':
+            # p=p.unsqueeze(0)
+            _, selected = probs.max(dim=-1)
+            selected = selected.unsqueeze(0)
+        elif self.select_type == 'sampling':
+            if isinstance(probs, torch.Tensor):
+
+                selected = probs.multinomial(1)
+                # visitflag[selected]=True
+            else:
+                selected = []
+                for item in probs:
+                    selected.append(item.multinomial(1).squeeze(1))
+                visitflag[selected] = True
+
+        return selected, visitflag
+class Attention_(nn.Module):
+
+    def __init__(self,n_embd=32,n_head=4):
+        super(Attention_, self).__init__()
+
+        self.n_embd=n_embd
+        self.n_head=n_head
+        self.key_=nn.Linear(n_embd,n_embd).to(device)
+        self.query_=nn.Linear(n_embd,n_embd).to(device)
+        self.value_=nn.Linear(n_embd,n_embd).to(device)
+
+        self.proj=nn.Linear(n_embd,n_embd)
+        self.proj2=nn.Linear(n_embd,n_embd)
+
+    # batch_size,num_agents,graph_size,hidden_dim
+    def forward(self,query,key,value):
+        B,L,G,D=key.size()
+
+        K=self.key_(key).view(B,L,self.n_head, G,D//self.n_head).transpose(1,2).transpose(2,3) # (B,nh,L,G,hdim) L=n_agents
+        V = self.value_(value).view(B, L, self.n_head,G, D // self.n_head).transpose(1, 2).transpose(2,3) # (B,nh,G,L,hdim)
+        Q = self.query_(query).view(B, L, self.n_head,G, D // self.n_head).transpose(1, 2).transpose(2,3)  # (B,nh,G,L,hdim)
+
+        att=torch.matmul(Q,K.transpose(-2,-1))/math.sqrt(D)  # (B,nh,G,L,L)
+        att=F.softmax(att,dim=-1)  # (B,nh,G,L,L)
+        y=torch.matmul(att,V)      #  (B,nh,G,L,hdim)
+        y=y.transpose(2,3).transpose(1,2).contiguous().view(B,L,G,D)
+        y=F.relu(self.proj(y))
+        y=self.proj2(y)
+        return y
+
+class SelfAttention_(nn.Module):
+
+    def __init__(self,n_embd=32,n_head=4,norm_shape=32):
+        super(SelfAttention_, self).__init__()
+
+        self.n_embd=n_embd
+        self.n_head=n_head
+        self.key_=nn.Linear(n_embd,n_embd).to(device)
+        self.query_=nn.Linear(n_embd,n_embd).to(device)
+        self.value_=nn.Linear(n_embd,n_embd).to(device)
+        self.ln=nn.LayerNorm(norm_shape)
+        self.proj=nn.Linear(n_embd,n_embd)
+        self.proj2 = nn.Linear(n_embd, n_embd)
+
+    def forward(self,query,key,value): # (batch_size,num_agent,hidden_dim)
+        B,L,D=key.size()
+
+        K=self.ln(key+self.key_(key)).view(B,L,self.n_head, D//self.n_head).transpose(1,2) # (B,nh,L,hdim) L=n_agents
+        V = self.ln(value+self.value_(value)).view(B, L, self.n_head, D // self.n_head).transpose(1, 2)  # (B,nh,L,hdim)
+        Q =self.ln(query +self.query_(query)).view(B, L, self.n_head, D // self.n_head).transpose(1, 2)  # (B,nh,1,hdim)
+
+        att=torch.matmul(Q,K.transpose(-2,-1))/math.sqrt(D)  # (B,nh,1,L)
+        att=F.softmax(att,dim=-1)  # (B,nh,L,L)
+        y=torch.matmul(att,V)      #  (B,nh,L,hdim)
+        y=y.transpose(1,2).contiguous().view(B,L,D)
+        y=F.relu(self.proj2(y))
+        y=self.ln(self.proj(y))
+        return y
+
+
+
+
